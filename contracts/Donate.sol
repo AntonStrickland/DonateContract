d8a36dca23d5a1f778ee6367b2b4e93f340e4db0
Kinjo <antonmstrickland@gmail.com>
2018-12-28 01:40:10 -0600

    Can donate and withdraw funds using the Donate contract

--- contracts/Donate.sol
+++ contracts/Donate.sol
@@ -2,5 +2,41 @@ pragma solidity ^0.4.17;
 
 contract Donate {
 
+  address public admin;
+
+  uint public currentBalance;
+  uint public totalBalance;
+  uint public numberOfDonations;
+
+  modifier adminOnly() {
+    require(msg.sender == admin);
+    _;
+  }
+
+  constructor() public {
+    admin = msg.sender;
+  }
+
+  function donate() public payable {
+
+    // Keep track of the balance
+    currentBalance += msg.value;
+    totalBalance += msg.value;
+
+    numberOfDonations++;
+  }
+
+  function withdraw() public adminOnly {
+
+    // Reset the tracked balance to zero
+    currentBalance = 0;
+
+    // Transfer the entire balance to the admin
+    admin.transfer(address(this).balance);
+  }
+
+  function getSummary() public view returns (uint, uint, uint) {
+    return ( currentBalance, totalBalance, numberOfDonations );
+  }
 
 }
